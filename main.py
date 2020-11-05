cd4796194ce2531cb8d7a143dd3c82122a8a4db7
Noah Zarro <noahzarro@gmail.com>
2020-11-05 16:04:13 +0100

    added cursed sloth

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ for file in os.listdir("sounds"):
 
 print(sounds)
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/', static_folder='public')
 
 @app.route('/')
 def hello_world():
