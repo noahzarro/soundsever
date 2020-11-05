a7f082eeed5fc39715edb53688d45dc70bd5600f
Noah Zarro <noahzarro@gmail.com>
2020-11-05 14:35:16 +0100

    created sound server

---
+++ main.py
@@ -0,0 +1,37 @@
+from playsound import playsound
+from flask import Flask
+
+import os
+import json
+
+sounds = []
+
+cur_id = 0
+
+for file in os.listdir("sounds"):
+    if file.endswith(".mp3"):
+        sounds.append({"Name": file.split(".")[0], "id": cur_id})
+        cur_id += 1
+
+print(sounds)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/sounds')
+def show_sounds():
+    return json.dumps(sounds)
+
+@app.route('/play/<id>')
+def play_sound(id):
+    sound_to_play = sounds[int(id)]["Name"]+".mp3"
+    print(sound_to_play)
+    playsound("sounds/"+sound_to_play)
+    return sounds[int(id)]["Name"]
+
+
+app.run(host="localhost", port=8080)
