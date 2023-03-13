9180985d6afd3fa56793d01ece8b5d295d71f4e7
Pascal Schärli <github@pascscha.ch>
2023-03-13 10:37:12 +0100

    Formatting, pep8 compliance

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from threading import Thread
 
 import os
 import json
+
 print("hello")
 sounds = []
 
@@ -14,28 +15,30 @@ for file in os.listdir("sounds"):
         sounds.append({"name": file.split(".")[0], "id": cur_id})
         cur_id += 1
 
-app = Flask(__name__, static_url_path='/', static_folder='public')
+app = Flask(__name__, static_url_path="/", static_folder="public")
+
 
 def play_sound_file(file):
     print("hello gitter")
     playsound(file)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, Gitter!'
+    return "Hello, Gitter!"
 
 
-@app.route('/sounds')
+@app.route("/sounds")
 def show_sounds():
     return json.dumps(sounds)
 
-@app.route('/play/<id>')
+
+@app.route("/play/<id>")
 def play_sound(id):
-    sound_to_play = sounds[int(id)]["name"]+".mp3"
-    t = Thread(target=play_sound_file, args=("sounds/"+sound_to_play,))
+    sound_to_play = sounds[int(id)]["name"] + ".mp3"
+    t = Thread(target=play_sound_file, args=("sounds/" + sound_to_play,))
     t.start()
     return sounds[int(id)]["name"]
-
 
 
 app.run(host="0.0.0.0", port=8080)
